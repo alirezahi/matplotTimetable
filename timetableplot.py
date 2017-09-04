1676de1a9d76019a7387e44c0a0dd5261d63cbe6
Alireza <alireza97hi@gmail.com>
2017-09-04 20:57:32 +0430

    arabic persian corrected added

--- timetableplot.py
+++ timetableplot.py
@@ -1,11 +1,19 @@
-  # !usr/bin/env python
-  # coding: utf-8
+
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+from bidi.algorithm import get_display
 import matplotlib.pyplot as plt
-rooms = ['Room A', 'Room B', 'Room C', 'Room D', 'Room E']
+import matplotlib.font_manager as fm
+import arabic_reshaper
+reshaped_text = arabic_reshaper.reshape(u'سلام')
+artext = get_display(reshaped_text)
+
+rooms = [artext, 'یکشنبه', 'دوشنبه', 'سه شنبه', 'چهارشنبه']
 colors = ['pink', 'lightgreen', 'lightblue', 'wheat', 'salmon']
 input_files = ['data1.txt']
 day_labels = ['Day 1']
-
+prop = fm.FontProperties(fname='IRANSansWeb_Medium.ttf')
 for input_file, day_label in zip(input_files, day_labels):
     fig = plt.figure(figsize=(10, 5.89))
     for line in open(input_file, 'r'):
@@ -16,30 +24,26 @@ for input_file, day_label in zip(input_files, day_labels):
         start = data[1] + data[2] / 60
         end = start + data[3] / 60
         # plot event
-        plt.fill_between([room, room + 0.96], [start, start], [end, end], color=colors[int(data[0] - 1)], edgecolor='k', linewidth=0.5)
+        plt.fill_between([room, room + 0.96], [start, start], [end, end],
+                         color=colors[int(data[0] - 1)], edgecolor='k', linewidth=0.5)
         # plot beginning time
-        plt.text(room + 0.02, start + 0.05, '{0}:{1:0>2}'.format(int(data[1]), int(data[2])), va='top', fontsize=7)
+        plt.text(room + 0.02, start + 0.05,
+                 '{0}:{1:0>2}'.format(int(data[1]), int(data[2])), va='top', fontsize=7, fontproperties=prop)
         # plot event name
-        plt.text(room + 0.48, (start + end) * 0.5, event, ha='center', va='center', fontsize=11)
-
-    # Set Axis
+        plt.text(room + 0.48, (start + end) * 0.5, event,
+                 ha='center', va='center', fontsize=11, fontproperties=prop)
     ax = fig.add_subplot(111)
     ax.yaxis.grid()
     ax.set_xlim(0.5, len(rooms) + 0.5)
     ax.set_ylim(15.1, 8.9)
-    ax.set_xticks(range(1, len(rooms) + 1))
-    ax.set_xticklabels(rooms)
+    ax.set_xticks(range(1, len(rooms) + 1), fontproperties=prop)
+    ax.set_xticklabels(rooms, fontproperties=prop)
     ax.set_ylabel('Time')
-
-    # Set Second Axis
     ax2 = ax.twiny().twinx()
     ax2.set_xlim(ax.get_xlim())
     ax2.set_ylim(ax.get_ylim())
     ax2.set_xticks(ax.get_xticks())
     ax2.set_xticklabels(rooms)
-    ax2.set_ylabel('Time')
-
-
-    plt.title(day_label, y=1.07)
-    plt.savefig('{0}.png'.format(day_label), dpi=200)
-    print('asdfsd')
+    ax2.set_ylabel('Time', fontproperties=prop)
+    plt.title('سلام', y=1.07, fontproperties=prop)
+    plt.savefig('bahbah.png', dpi=200)
